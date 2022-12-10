9d475b4b4e69c6abb1051f44318feeb1fd160967
ariddl <watchoutmayhem@gmail.com>
2022-12-09 22:48:51 -0500

    RenameModule prototype that renames all method calls

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/modules/RenameModule.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/modules/RenameModule.java
@@ -1,6 +1,7 @@
 package com.theobfuscatorinator.modules;
 
 import java.util.Random;
+import java.util.HashMap;
 
 import com.github.javaparser.ast.*;
 import com.github.javaparser.ast.body.*;
@@ -14,9 +15,7 @@ import com.github.javaparser.ast.stmt.Statement;
 import com.github.javaparser.ast.visitor.ModifierVisitor;
 import com.github.javaparser.ast.visitor.Visitable;
 
-public class RenameModule extends ModifierVisitor<Void> implements IModule {
-    private static Random rnd = new Random();
-
+public class RenameModule implements IModule {
     @Override
     public String getName() {
         return "Rename";
@@ -29,18 +28,20 @@ public class RenameModule extends ModifierVisitor<Void> implements IModule {
 
     @Override
     public void execute(Context ctx) {
+        HashMap<String, String> renamedMethods = new HashMap<>();
+
+        for (Node node : ctx.currentCU.getChildNodes()) {
+            if (node instanceof ClassOrInterfaceDeclaration) {
+            }
+        }
+
+        // Apply a visitor to update the references to the things we renamed (e.g., methods)
         ctx.currentCU.accept(new ModifierVisitor<Void>() {
             @Override
-            public Visitable visit(IfStmt n, Void arg) {
-                n.getCondition().ifBinaryExpr(binExpr -> {
-                    if (binExpr.getOperator() == BinaryExpr.Operator.NOT_EQUALS && n.getElseStmt().isPresent()) {
-                        Statement thenStmt = n.getThenStmt().clone();
-                        Statement elseStmt = n.getElseStmt().get().clone();
-                        n.setThenStmt(elseStmt);
-                        n.setElseStmt(thenStmt);
-                        binExpr.setOperator(BinaryExpr.Operator.EQUALS);
-                    }
-                });
+            public Visitable visit(MethodCallExpr n, Void arg) {
+                SimpleName name = new SimpleName();
+                name.setIdentifier(ModuleUtils.randName(8));
+                n.setName(name);
                 return super.visit(n, arg);
             }
         }, null);
