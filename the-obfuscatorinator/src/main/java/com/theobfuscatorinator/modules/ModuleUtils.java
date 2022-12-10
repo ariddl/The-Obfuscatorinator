6d6cb0b892cd3ca67f88cf034452c00c0e8d9aca
ariddl <watchoutmayhem@gmail.com>
2022-12-09 21:52:22 -0500

    Add comments and find class/interface to ModuleUtils

--- the-obfuscatorinator/src/main/java/com/theobfuscatorinator/modules/ModuleUtils.java
+++ the-obfuscatorinator/src/main/java/com/theobfuscatorinator/modules/ModuleUtils.java
@@ -2,13 +2,36 @@ package com.theobfuscatorinator.modules;
 
 import java.nio.file.Path;
 import java.nio.file.Paths;
+import java.util.ArrayList;
 
 import com.github.javaparser.utils.CodeGenerationUtils;
+import com.github.javaparser.ast.CompilationUnit;
+import com.github.javaparser.ast.Node;
+import com.github.javaparser.ast.body.ClassOrInterfaceDeclaration;
 
 final class ModuleUtils {
-    static Path resolvePath(String value) {
-        return !value.startsWith("$")
-            ? Paths.get(value)
-            : CodeGenerationUtils.mavenModuleRoot(ModuleObfuscator.class).resolve(value.substring(1));
+    /*
+        Resolve a path, either relative (if path starts with $) or absolute.
+        Relative meaning relative to the maven project.
+    */
+    static Path resolvePath(String path) {
+        return !path.startsWith("$")
+            ? Paths.get(path)
+            : CodeGenerationUtils.mavenModuleRoot(ModuleObfuscator.class).resolve(path.substring(1));
+    }
+
+    /*
+        Find a list of class or interface declarations. There could be multiple
+        in theory if the user has two top-level classes. This is a useful utility
+        for when we want to modify classes by adding methods, etc.
+    */
+    static Node findClassOrInterfaceDeclaration(CompilationUnit cu) {
+        ArrayList<Node> nodes = new ArrayList<>();
+        for (Node node : cu.getChildNodes()) {
+            if (node instanceof ClassOrInterfaceDeclaration) {
+                nodes.add((ClassOrInterfaceDeclaration)node);
+            }
+        }
+        return nodes;
     }
 }
